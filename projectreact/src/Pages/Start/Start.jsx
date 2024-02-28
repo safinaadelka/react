a20f9f6c00c62c151c3eac7285fc7a4974685d9a
safinaadelka <lodgevvv@gmail.com>
2024-02-28 09:52:59 +0300

    sm12

--- projectreact/src/Pages/Start/Start.jsx
+++ projectreact/src/Pages/Start/Start.jsx
@@ -1,5 +1,6 @@
 import Banner from '../../components/Banner/Banner'
 import SliderS from '../../components/Sliders/Sliders'
+import Accordion from '../../components/Accordion/Accrodion'
 
 export default function Start (){
 
@@ -8,6 +9,11 @@ export default function Start (){
         <>
         <Banner />
         <SliderS />
+        <Accordion title="Вопрос 1" content="Ответы на вопрос 1"/>
+        <Accordion title="Вопрос 2" content="Ответы на вопрос 2"/>
+        <Accordion title="Вопрос 3" content="Ответы на вопрос 3"/>
+        <br />    <br /> 
+        <br />    <br /> 
         </>
 
     ) 
